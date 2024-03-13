3d57f9d32b4b0192ec7f2a7a78ffdc20bf2e4abf
Xu Ruixin <rx407@nyu.edu>
2024-03-13 15:20:38 -0700

    finished

--- obj/Debug/net8.0/sqlServerDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/sqlServerDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sqlServerDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8fc99be08e3c4b88c71f1093dda6cceaf70230e")]
 [assembly: System.Reflection.AssemblyProductAttribute("sqlServerDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sqlServerDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
